a23a49696a54eb1241ec73d6c27237e7e978608d
Viramat <virasisombat@yahoo.fr>
2021-04-02 15:40:32 +0200

    Modification index.php redirection page connexion

--- index.php
+++ index.php
@@ -1,6 +1,2 @@
 <?php
-if (!empty($_SESSION["idrole"]) && $_SESSION["idrole"] == 2) {
-    header("location:admin/index.php");
-} else {
-    header("location:membre/index.php");
-}
+header("location:pages/connexion.php");
